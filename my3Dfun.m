2fe60ada9131461f522bd5b2b203ce2ea53f45da
DESKTOP-R3B0IJT\Michaela Horváthová <xhorvathovam@stuba.sk>
2018-11-26 13:28:27 +0100

    my3D1funB

--- my3Dfun.m
+++ my3Dfun.m
@@ -6,16 +6,16 @@ z_a=x_a.*exp(-x_a.^2 - y_a.^2);
 % Figure initialization
 figure
 % Plotting figure
-surf(x_a,y_a,z_a)
+surfc(x_a,y_a,z_a)
 % Title
-title('z_A = x exp(-x^2 - y^2)')
+title('z_B = x exp(-x^2 - y^2)')
 % Labels
-xlabel('y_A')
-ylabel('x_A')
-zlabel('z_A')
+xlabel('y_B')
+ylabel('x_B')
+zlabel('z_B')
 % Grid
-grid on
+grid minor
 % Colormap
-colormap('default')
+colormap('cool')
 % Shading
 shading INTERP
